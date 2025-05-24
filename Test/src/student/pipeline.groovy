bb1deddac2dbce018158c15af0c1be93d08a6828
이지윤 <118089974+leejiyoon223@users.noreply.github.com>
2025-05-24 13:42:02 +0900

    Update pipeline.groovy
    34번째 줄에 cd jenkisTest 삭제해봄

--- Test/src/student/pipeline.groovy
+++ Test/src/student/pipeline.groovy
@@ -31,7 +31,6 @@ pipeline {
             steps {
                 sh '''
                     echo "[+] Compiling source files..."
-                    cd jenkisTest
                     find src -name "*.java" > sources.txt
                     javac -encoding UTF-8 -d ../${CLASS_DIR} -cp ../${JUNIT_JAR_PATH} @sources.txt
                 '''
