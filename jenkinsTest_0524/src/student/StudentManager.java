c71fe56687051dcf7919057a61ea20f1810d050e
leejiyoon223 <118089974+leejiyoon223@users.noreply.github.com>
2025-05-24 14:37:32 +0900

    add studentManager JunitTest

--- jenkinsTest_0524/src/student/StudentManager.java
+++ jenkinsTest_0524/src/student/StudentManager.java
@@ -21,6 +21,7 @@ public class StudentManager {
         }
         students.remove(name);
     }
+    
     //학생 조회(boolean타입 반환)
     public boolean hasStudent(String name) {
         return students.contains(name);
